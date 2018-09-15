92384c67e56d2585009d18088e175aa9e5e84040
jhonjaner11 <jhonjaner11@gmail.com>
2018-09-14 21:02:45 -0500

    holi

---
+++ arbolSUPER.py
@@ -0,0 +1,15 @@
+# Imports needed for the script
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+#%matplotlib inline
+plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from IPython.display import Image as PImage
+from subprocess import check_call
+from PIL import Image, ImageDraw, ImageFont
